8e6ae22bb69128bb33a04acd6aeae65a9a59abe8
Rayyyyyyyyyyyyyyyyyyyy <73267445+Rayyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2021-01-15 13:30:11 +0800

    add search

--- routes/ProductsList.js
+++ routes/ProductsList.js
@@ -54,48 +54,26 @@ conn.query(sql,[], function (err, rows) {
     
 });
 
-// router.post(`/create`, function (req, res, next) {
-//     let sql1 = `INSERT INTO travelbuddies themeName = ?,
-//     owner_id = ?,
-//     themePhoto = ?,
-//     dateBegin = ?,
-//     dateEnd = ?, 
-//     daysCategory_id = ?, 
-//     lastApprovedDate = ?, 
-//     personNeeded = ?, 
-//     genderNeeded = ?, 
-//     estimatedCost = ?, 
-//     themeIntro = ?, 
-//     valid = ? `
-//     let sql2 = `INSERT INTO categoryRelations travelbuddies_id = ?,
-//     regionCategory_id  =  ?,
-//     cityCategory_id = ?`
-//     conn.beginTransaction
-//     res.send(`已連線`)
-    
-// })
 
-// router.put(`/update`, function (req, res, next) {
-//     let sql1 = ` travelbuddies themeName  =  ?,
-//     owner_id = ?, 
-//     themePhoto = ?, 
-//     dateBegin = ?, 
-//     dateEnd = ?, 
-//     daysCategory_id = ?, 
-//     lastApprovedDate = ?, 
-//     personNeeded = ?, 
-//     genderNeeded = ?, 
-//     estimatedCost = ?, 
-//     themeIntro = ?, 
-//     valid = ? `
-//     let sql2 = `INSERT INTO categoryRelations travelbuddies_id = ?,
-//     regionCategory_id = ?,
-//     cityCategory_id = ?`
-//     conn.beginTransaction
-//     res.send(`已連線`)
-    
-// })
-
+router.get(`/car1/:id`, function (req, res, next) {
+  let productId = req.params.id;
+  console.log("hi");
+  let sql = `SELECT products.*,
+    teacher.name as teacher_name,
+    teacher.title as teacher_title,
+    teacher.photo as teacher_photo,
+    teacher.history as teacher_history
+    from products
+    join teacher on products.teacher_id = teacher.id
+    where products.id = ${productId}
+   `;
+  conn.query(sql, [], function (err, rows) {
+    if (err) {
+      console.log(err);
+    }
+    res.send(JSON.stringify(rows));
+  });
+});
 
 
 module.exports  =  router;
