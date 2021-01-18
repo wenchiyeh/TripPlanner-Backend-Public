6ebc12f9c9b1fe973dc68a29e8a2712752961e89
kkshen <h73423352@gmail.com>
2021-01-18 14:43:07 +0800

    註冊ok

--- routes/member.js
+++ routes/member.js
@@ -18,6 +18,11 @@ router.get("/", function (req, res, next) {
   let sqlKey = `SELECT member.*,
   email,
   password,
+  member_name,
+  phone,
+  member_sex,
+  member_id,
+  member_aboutme
   newsId='${newsId}'
   from member where valid=1`;
   //let sqlKey = `select * from member email='${req.body.email}' and password='${req.body.password}' and newsId='${newsId}'`;
@@ -46,6 +51,67 @@ router.get("/:id", function (req, res, next) {
   const obj = JSON.parse(JSON.stringify(req.params)); // req.body = [Object: null prototype] { title: 'product' }
   //這樣寫才對
   console.log('/member:id',obj);
+  conn.query(sqlKey,[], function (err, rows) {
+    if(err){
+        console.log(err);
+    }
+        res.send(JSON.stringify(rows));
+        });
+    
+});
+
+
+// 更新會員資料
+router.put("/", function (req, res, next) {
+  let { id: newsId } = req.body
+// update statment
+let sqlKey = `update member set 
+    email='${req.body.email}', 
+    password='${req.body.password}', 
+    member_name='${req.body.member_name}',  
+    member_phone='${req.body.member_phone}', 
+    member_sex='${req.body.member_sex}',
+    member_id='${req.body.member_id}',
+    member_aboutme='${req.body.member_aboutme}' where newsId=${newsId} or valid=1'`;
+
+    const obj = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
+  //這樣寫才對
+  console.log('/ud1',obj);
+  
+  conn.query(sqlKey,[], function (err, rows) {
+    if(err){
+        console.log(err);
+    }
+        res.send(JSON.stringify(rows));
+        });
+    
+});
+
+
+
+router.put("/:id", function (req, res, next) {
+  console.log("udid:",req.params.newsId)
+  let sqlKey = `update member set 
+    email=?, 
+    password=?, 
+    member_name=?,  
+    member_phone=?,
+    member_sex=?,
+    member_id=?,
+    member_aboutme=?
+    where email='${req.params.email}' or newsId=${newsId}'`;
+  //   email='${req.body.email}', 
+  //   password='${req.body.password}', 
+  //   member_name='${req.body.member_name}',  
+  //   member_phone='${req.body.member_phone}',  
+  //   birthday='${req.body.birthday}', 
+  //   member_sex='${req.body.member_sex}',
+  //   member_id='${req.body.member_id}',
+  //   member_aboutme='${req.body.member_aboutme}' where newsid='${req.body.newsId}'`;
+  const obj = JSON.parse(JSON.stringify(req.params)); // req.body = [Object: null prototype] { title: 'product' }
+  //這樣寫才對
+  console.log('/ud2',obj);
+  
   conn.query(sqlKey,[], function (err, rows) {
     if(err){
         console.log(err);
