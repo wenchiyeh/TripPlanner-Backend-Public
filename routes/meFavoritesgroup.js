a3f9d70d54fcbd97f333910335a885d66ab336ec
kkshen <h73423352@gmail.com>
2021-01-21 19:05:16 +0800

    存檔

--- routes/meFavoritesgroup.js
+++ routes/meFavoritesgroup.js
@@ -13,20 +13,60 @@ var conn = mysql.createConnection({
 });
 
 //我的揪團
-router.get("/", function (req, res, next) {
-    let sql =`SELECT travelbuddies.id, travelbuddies.themeName AS tb_themeName,
-     travelbuddies.dateBegin AS tb_dateBegin ,
-      travelbuddies.dateEnd AS tb_dateEnd ,
-      area.region AS tb_region
-      FROM travelbuddies 
-      JOIN categoryRelations ON categoryRelations. travelBuddies_id=travelbuddies.id 
-      JOIN cityCategory ON categoryRelations.cityCategory_id=cityCategory.id 
-      JOIN regionCategory ON cityCategory.regionCategory_id=regionCategory.id 
-      JOIN dayscategory ON travelbuddies.daysCategory_id=daysCategory.id 
-      JOIN area
-      WHERE travelbuddies.owner_id = 1 AND travelbuddies.dateEnd>=NOW() 
-      GROUP BY travelBuddies.themeName 
-      ORDER BY travelBuddies.id;`
+// router.get("/", function (req, res, next) {
+//     let sql =`SELECT travelbuddies.id, travelbuddies.themeName AS tb_themeName,
+//     travelbuddies.dateBegin AS tb_dateBegin,
+//     travelbuddies.dateEnd AS tb_dateEnd,
+//     memberssignedup.membersStatus AS tb_membersStatus,
+//     regioncategory.region AS tb_region,
+//     citycategory.city AS tb_city
+//     FROM travelbuddies 
+//     INNER JOIN categoryrelations
+//     INNER JOIN citycategory
+//     JOIN regioncategory
+//     JOIN dayscategory ON travelbuddies.daysCategory_id=daysCategory.id 
+//     JOIN memberssignedup ON travelbuddies.id=memberssignedup.travelBuddies_id 
+//     WHERE memberssignedup.members_id = 1 AND travelbuddies.dateEnd>NOW() 
+//     GROUP BY travelBuddies.themeName 
+//     ORDER BY travelBuddies.id;`
+    
+    // `select categoryRelations.*, 
+    //     citycategory.city as city, 
+    //     regioncategory.region as region from categoryRelations
+    //     JOIN cityCategory ON categoryRelations.cityCategory_id=cityCategory.id 
+    //     JOIN regionCategory ON cityCategory.regionCategory_id=regionCategory.id 
+    //     ON categoryRelations.travelBuddies_id=travelbuddies.id;`
+
+// conn.query(sql,[], function (err, rows) {
+//     if(err){
+//         console.log(err);
+//     }
+//         res.send(JSON.stringify(rows));
+//         });
+    
+// });
+router.post("/", function (req, res, next) {
+    let sql =`SELECT travelbuddies.id, member.member_name AS tb_owner,
+     travelbuddies.themeName AS tb_themeName,
+      travelbuddies.personsNeeded AS tb_personsNeeded,
+      travelbuddies.genderNeeded AS tb_genderNeeded,
+       travelbuddies.estimatedCost AS tb_estimatedCost,
+       travelbuddies.lastApprovedDate AS tb_lastApprovedDate,
+        travelbuddies.dateBegin AS tb_dateBegin ,
+        travelbuddies.dateEnd AS tb_dateEnd,
+         daysCategory.daysCategory AS tb_daysCategory,
+         GROUP_CONCAT(DISTINCT regionCategory.region ORDER BY regionCategory.id) AS tb_region ,
+         GROUP_CONCAT(DISTINCT cityCategory.city ORDER BY cityCategory.id) AS tb_city,
+           travelBuddies.themeintro AS tb_themeIntro FROM travelbuddies J
+           OIN member ON travelbuddies.owner_id=member.newsId 
+           JOIN categoryRelations ON categoryRelations. travelBuddies_id=travelbuddies.id 
+           JOIN cityCategory ON categoryRelations.cityCategory_id=cityCategory.id 
+           JOIN regionCategory ON cityCategory.regionCategory_id=regionCategory.id 
+           JOIN dayscategory ON travelbuddies.daysCategory_id=daysCategory.id 
+           WHERE travelbuddies.id = 19 
+           GROUP BY travelBuddies.themeName 
+           ORDER BY travelBuddies.id;`
+           
 conn.query(sql,[], function (err, rows) {
     if(err){
         console.log(err);
