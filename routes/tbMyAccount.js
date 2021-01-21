8b210e7cbd162e46ecf0af794e54fb22b66c21c0
kkshen <h73423352@gmail.com>
2021-01-21 22:08:07 +0800

    後端ok

--- routes/tbMyAccount.js
+++ routes/tbMyAccount.js
@@ -30,7 +30,19 @@ conn.query(sql,[], function (err, rows) {
 
 
 router.get("/tbjoined", function (req, res, next) {
-    let sql ="SELECT travelbuddies.id, travelbuddies.themeName AS tb_themeName, travelbuddies.dateBegin AS tb_dateBegin , travelbuddies.dateEnd AS tb_dateEnd, memberssignedup.membersStatus AS tb_membersStatus FROM travelbuddies JOIN categoryRelations ON categoryRelations. travelBuddies_id=travelbuddies.id JOIN cityCategory ON categoryRelations.cityCategory_id=cityCategory.id JOIN regionCategory ON cityCategory.regionCategory_id=regionCategory.id JOIN dayscategory ON travelbuddies.daysCategory_id=daysCategory.id JOIN memberssignedup ON travelbuddies.id=memberssignedup.travelBuddies_id WHERE memberssignedup.members_id = 1 AND travelbuddies.dateEnd>NOW() GROUP BY travelBuddies.themeName ORDER BY travelBuddies.id;"
+    let sql =`SELECT travelbuddies.id, travelbuddies.themeName AS tb_themeName,
+     travelbuddies.dateBegin AS tb_dateBegin ,
+      travelbuddies.dateEnd AS tb_dateEnd,
+       memberssignedup.membersStatus AS tb_membersStatus 
+       FROM travelbuddies 
+       JOIN categoryRelations ON categoryRelations. travelBuddies_id=travelbuddies.id 
+       JOIN cityCategory ON categoryRelations.cityCategory_id=cityCategory.id 
+       JOIN regionCategory ON cityCategory.regionCategory_id=regionCategory.id 
+       JOIN dayscategory ON travelbuddies.daysCategory_id=daysCategory.id 
+       JOIN memberssignedup ON travelbuddies.id=memberssignedup.travelBuddies_id 
+       WHERE memberssignedup.members_id = 1 AND travelbuddies.dateEnd>NOW() 
+       GROUP BY travelBuddies.themeName 
+       ORDER BY travelBuddies.id;`
 conn.query(sql,[], function (err, rows) {
     if(err){
         console.log(err);
