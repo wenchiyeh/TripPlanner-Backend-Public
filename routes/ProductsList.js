6f31a2b5971f8d522a38729e4dbae17adde92fe8
Rayyyyyyyyyyyyyyyyyyyy <73267445+Rayyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2021-01-25 15:55:17 +0800

    carousel

--- routes/ProductsList.js
+++ routes/ProductsList.js
@@ -3,6 +3,20 @@ var router = express.Router();
 var mysql = require("mysql");
 var conn = require("../dbConnect");
 
+
+
+router.get("/product_carousel", function (req, res, next) {
+  let sql =
+    "SELECT products.id, products.classPhoto AS classPhoto, products.className AS className FROM products ORDER BY products.id DESC LIMIT 5;";
+  conn.query(sql, [], function (err, rows) {
+    if (err) {
+      console.log(err);
+    }
+    res.send(JSON.stringify(rows));
+  });
+});
+
+
 router.get(`/:id`, function (req, res, next) {
   let productId = req.params.id;
   let sql = `SELECT products.*,
@@ -37,57 +51,7 @@ router.get(`/car1/:id`, function (req, res, next) {
 });
 
 router.get(`/`, function (req, res, next) {
-  let { area = "", town = "", day = 0, keyword = "" } = req.query;
-  area = area.replace("全部", "");
-  town = town.replace("全部", "");
-  keyword = keyword.replace(/[\/.,{}\[\]()=*%$#@!&|]/g, "");
-  let dayOption = `and itinerary.duration `;
-  switch (day) {
-    case 0:
-      dayOption = "";
-      break;
-    case 1:
-      dayOption += "= 1";
-      break;
-    case 2:
-      dayOption += "between 2 and 3";
-      break;
-    case 3:
-      dayOption += "between 4 and 5";
-      break;
-    case 4:
-      dayOption += "between 6 and 7";
-      break;
-    case 5:
-      dayOption += "> 7";
-      break;
-  }
-
-  if (area !== "") area = `products.address = '${area}' `;
-  if (town !== "") town = `products.classCity = '%${town}%' `;
-  if (keyword !== "") {
-    keyword = `products.className like '%${keyword}%' `;
-  }
-
-  let filter = [area, town, keyword];
-  let filterStr = "";
-  let handleFilter = "";
-  filter.forEach((ele) => {
-    if (ele == "") return;
-    filterStr += ` ${ele}`;
-  });
-  handleFilter = `
-  SELECT products.*,
-  teacher.name as teacher_name,
-  teacher.title as teacher_title,
-  teacher.photo as teacher_photo,
-  teacher.history as teacher_history
-  from products
-  join teacher on products.teacher_id = teacher.id
-  where ${filterStr} 
-  ORDER BY products.id
-  `;
-  console.log(handleFilter);
+  
   let sql = `SELECT products.*,
   teacher.name as teacher_name,
   teacher.title as teacher_title,
@@ -97,7 +61,6 @@ router.get(`/`, function (req, res, next) {
   join teacher on products.teacher_id = teacher.id
   ORDER BY products.id
    `;
-  area === "" && town === "" && keyword === "" ? sql : handleFilter;
   conn.query(sql, [], function (err, rows) {
     if (err) {
       console.log(err);
@@ -105,5 +68,8 @@ router.get(`/`, function (req, res, next) {
     res.send(JSON.stringify(rows));
   });
 });
+
+
+
 
 module.exports = router;
